5282d6a2188f34bcd7080103d4323a5ce3807d7d
Mihai Zamfir <mihai.zamfir@eaudeweb.ro>
2014-07-14 18:23:13 +0300

    scraper: create parser for tenders list.

--- scratch/scraper.py
+++ scratch/scraper.py
@@ -1,10 +1,19 @@
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+
+CSS_ROW_CLASS_NAME = 'tableRow dataRow'
+ENDPOINT_URI = 'https://www.ungm.org'
+
+
+def string_to_date(date):
+    return datetime.strptime(date, '%d-%b-%Y').date()
 
 
 def parse_tender(html):
     """ Parse a tender HTML and return a dictionary with information such
      as: title, description, published etc
     """
-    return {}
 
 
 def parse_award(html):
@@ -14,7 +23,16 @@ def parse_award(html):
 
 
 def parse_tender_list(html):
-    """ Parse a list of tenders and return a list of dictionaries.
-    Example: [{'title': '...', 'published': '', ...}, ...]
+    """ Parse a list of tenders and return a list of uris.
     """
-    return []
+
+    soup = BeautifulSoup(html)
+    rows = soup.find_all('div', CSS_ROW_CLASS_NAME)
+    tender_list = []
+
+    for row in rows:
+        href = row.contents[3].a['href']
+        url = ENDPOINT_URI + unicode(href) + '.html'
+        tender_list.append(url)
+
+    return tender_list

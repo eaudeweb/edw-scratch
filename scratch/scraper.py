715dbbb386de7eb5a740c7bd2c24cd81d7d6a363
Mihai Zamfir <mihai.zamfir@eaudeweb.ro>
2014-07-15 12:09:53 +0300

    scraper: create parser for tender details.

--- scratch/scraper.py
+++ scratch/scraper.py
@@ -2,8 +2,10 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 
 
-CSS_ROW_CLASS_NAME = 'tableRow dataRow'
+CSS_ROW_LIST_NAME = 'tableRow dataRow'
+CSS_ROW_DETAIL_NAME = 'reportRow'
 ENDPOINT_URI = 'https://www.ungm.org'
+DOWNLOAD_PATH = '/UNUser/Documents/DownloadPublicDocument?docId='
 
 
 def string_to_date(date):
@@ -15,19 +17,36 @@ def parse_tender(html):
      as: title, description, published etc
     """
 
+    soup = BeautifulSoup(html)
+    details = soup.find_all('div', CSS_ROW_DETAIL_NAME)
+    documents = soup.find_all('div', 'docslist')[0].find_all('div', 'filterDiv')
 
-def parse_award(html):
-    """ Parse a contract award HTML
-    """
-    return {}
+    tender = {
+        'title': details[2].span.string,
+        'organization': details[3].span.string,
+        'reference': details[4].span.string,
+        'published': string_to_date(details[5].span.string),
+        'deadline': string_to_date(details[6].span.string.split(' ')[0]),
+        'documents': [
+            {
+                'name': document.span.string,
+                'download_url': (
+                    ENDPOINT_URI + DOWNLOAD_PATH + document['data-documentid']
+                )
+            }
+            for document in documents
+        ]
+    }
+
+    return tender
 
 
 def parse_tender_list(html):
-    """ Parse a list of tenders and return a list of uris.
+    """ Parse a list of tenders and return a list of URLs.
     """
 
     soup = BeautifulSoup(html)
-    rows = soup.find_all('div', CSS_ROW_CLASS_NAME)
+    rows = soup.find_all('div', CSS_ROW_LIST_NAME)
     tender_list = []
 
     for row in rows:
@@ -36,3 +55,9 @@ def parse_tender_list(html):
         tender_list.append(url)
 
     return tender_list
+
+
+def parse_award(html):
+    """ Parse a contract award HTML
+    """
+    return {}

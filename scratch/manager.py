5818cdadb47372144adcd29dd1136a14b51ecb33
Mihai Zamfir <mihai.zamfir@eaudeweb.ro>
2014-07-18 11:48:47 +0300

    manager: add a command for adding tender data to DB

--- scratch/manager.py
+++ scratch/manager.py
@@ -2,10 +2,11 @@ import pprint
 
 from flask.ext.script import Manager
 
-from models import db_manager
+from models import db, db_manager, Tender, TenderDocument
 
 
 scrap_manager = Manager()
+add_manager = Manager()
 pp = pprint.PrettyPrinter(indent=4)
 
 
@@ -13,6 +14,7 @@ def create_manager(app):
     manager = Manager(app)
     manager.add_command('scrap', scrap_manager)
     manager.add_command('db', db_manager)
+    manager.add_command('add', add_manager)
 
     return manager
 
@@ -51,3 +53,31 @@ def parse_winner_html(filename):
         data = fin.read()
 
         pp.pprint(parse_winner(data))
+
+
+def add_tenders_from_html(html):
+    from scratch.scraper import parse_tender
+    tender = parse_tender(html)
+    tender_entry = Tender(
+        reference=tender['reference'],
+        title=tender['title'],
+        organization=tender['organization'],
+        published=tender['published'],
+        deadline=tender['deadline'],
+    )
+    for document in tender['documents']:
+        document_entry = TenderDocument(
+            name=document['name'],
+            download_url=document['download_url'],
+            tender=tender_entry,
+        )
+        db.session.add(document_entry)
+    db.session.add(tender_entry)
+    db.session.commit()
+
+
+@add_manager.command
+def add_tenders(filename):
+    with open(filename, 'r') as fin:
+        html = fin.read()
+    add_tenders_from_html(html)

b0751f769210e6b7f7fe1e222110b3c5a2759b1a
Mihai Zamfir <mihai.zamfir@eaudeweb.ro>
2014-08-05 13:07:47 +0300

    requests: refactor code - function calls

--- scratch/server_requests.py
+++ scratch/server_requests.py
@@ -12,39 +12,31 @@ def replace_endpoint(url):
     return url.replace(LIVE_ENDPOINT_URI, LOCAL_ENDPOINT_URI)
 
 
-def get_response(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.content
-
-
 def request_tenders_list(public):
     url = TENDERS_ENDPOINT_URI
     if not public:
-        url = replace_endpoint(url)
         url += '/tender_notices.html'
-    else:
-        pass
 
-    return get_response(url)
+    return request(url, public)
 
 
 def request_winners_list(public):
     url = WINNERS_ENDPOINT_URI
     if not public:
-        url = replace_endpoint(url)
         url += '/contract_winners.html'
-    else:
-        pass
 
-    return get_response(url)
+    return request(url, public)
 
 
 def request(url, public):
     if not public:
         url = replace_endpoint(url)
 
-    return get_response(url)
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+
+    return None
 
 
 def request_document(url, public):

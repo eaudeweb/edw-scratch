10c02b976295eaaac59f52497ea42212f54f9305
Mihai Zamfir <mihai.zamfir@eaudeweb.ro>
2014-08-06 16:42:32 +0300

    mails: change the content with which mails are filled

--- scratch/mails.py
+++ scratch/mails.py
@@ -1,24 +1,43 @@
-from flask.ext.mail import Mail, Message
 import smtplib
 
+from flask.ext.mail import Mail, Message
+from flask import current_app as app
+
 from server_requests import request_document
+from instance.settings import NOTIFY_EMAILS
 
 
-def send_email(subject, sender, recipients, html_body, tenders, public):
-    from flask import current_app as app
+def attach(msg, documents, public, index=None):
+    index = '' if index is None else '%s.' % index
+    for document in documents:
+        attachement = request_document(document['download_url'], public)
+        if attachement:
+            msg.attach(
+                index + '%s' % document['name'].replace(' ', '_'),
+                'application/unknown',
+                attachement
+            )
+
+
+def send_mail(subject, html_body, public, tenders=None, sender='Eau De Web',
+              recipients=NOTIFY_EMAILS):
+
+    msg = Message(
+        subject=subject,
+        recipients=recipients,
+        html=html_body,
+        sender=sender,
+    )
+    if not tenders:
+        pass
+    elif len(tenders) == 1:
+        tender = tenders.pop()
+        attach(msg, tender['documents'], public)
+    elif len(tenders) > 1:
+        for (index, tender) in enumerate(tenders):
+            attach(msg, tender['documents'], public, index)
 
     mail = Mail(app)
-    msg = Message(subject, sender=sender, recipients=recipients)
-    msg.html = html_body
-    for (index, tender) in tenders:
-        for document in tender['documents']:
-            attachement = request_document(document['download_url'], public)
-            if attachement:
-                msg.attach(
-                    '%s.%s' % (index, document['name'].replace(' ', '_')),
-                    'application/unknown',
-                    attachement
-                )
     try:
         mail.send(msg)
     except smtplib.SMTPAuthenticationError:
